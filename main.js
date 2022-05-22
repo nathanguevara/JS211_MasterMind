9d1b03c8a485019b356496bea18d25b8760cb57d
nathanguevara <nathan.guevara@gmail.com>
2022-05-21 19:34:50 -0500

    tests passing

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+var colors = require('colors');
+const { Console } = require('console');
 
 let board = [];
 let solution = '';
+solution = 'abcd';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 const printBoard = () =>  {
@@ -28,21 +31,62 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+  let soultionArray = solution.split('');
+  let guessArray = guess.split('');
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
+
+  // Checing for Correct Letter Location
+  for(let i = 0; i < guessArray.length; i++) {
+    if(guessArray[i] == soultionArray[i]){
+      correctLetterLocations++;
+      soultionArray[i] = null;
+    }
+  }
+
+  //Checking for Correct Letters
+  for(let j = 0; j < guessArray.length; j++) {
+    
+    let targetIndex;
+    targetIndex = soultionArray.indexOf(guessArray[j]);
+    if(targetIndex > -1) {
+      correctLetters++;
+      soultionArray[targetIndex] = null;
+    }
+  }
+  console.log(colors.red(correctLetterLocations) + " - " + correctLetters);
+  return correctLetterLocations + "-" + correctLetters;
 }
 
 const mastermind = (guess) => {
   solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
+  if(guess === solution) {
+    console.log('You guessed it!');
+    return 'You guessed it!';
+  }else {board.push(guess + " " + generateHint(guess));}
+  
 }
 
+const restartGame = () => {
+  board = [];
+  solution = '';
+  getPrompt();
+}
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
     printBoard();
-    getPrompt();
+    if(board.length < 10){
+      console.log("Guess again.")
+      getPrompt();
+    } else {
+        console.log(`You ran out of turns! The solution was ${solution}`);
+        restartGame();
+    }
   });
 }
 
